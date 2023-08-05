24d35a975a6a1ee5bde0d81dad1ce6b1f22cb352
avach <avachaspati97@gmail.com>
2023-08-05 20:20:12 +0530

    Added Chat Input

--- src/artemusChat/artemusChatPanelProvider.ts
+++ src/artemusChat/artemusChatPanelProvider.ts
@@ -22,7 +22,8 @@ export class ArtemusChatPanelProvider implements vscode.WebviewViewProvider {
 			enableScripts: true,
 
 			localResourceRoots: [
-                vscode.Uri.joinPath(this._extensionUri,"media/")
+                vscode.Uri.joinPath(this._extensionUri,"media/"),
+                vscode.Uri.joinPath(this._extensionUri,"out/compiled")
 			]
 		};
 
@@ -51,42 +52,37 @@ export class ArtemusChatPanelProvider implements vscode.WebviewViewProvider {
 			this.view.webview.postMessage({ type: 'clearColors' });
 		}
 	}
-
 	private _getHtmlForWebview(webview: vscode.Webview) {
+		
 		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-		// const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'out/compiled', 'artemusChatPanel.js'));
+		const customCSSUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'out/compiled', 'artemusChatPanel.css'));
+		
 		// Do the same for the stylesheet.
 		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
 		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
 		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
-
+		
 		// Use a nonce to only allow a specific script to be run.
 		const nonce = getNonce();
-
+		
 		return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-
-				<!--
-					Use a content security policy to only allow loading styles from our extension directory,
-					and only allow scripts that have a specific nonce.
-					(See the 'webview-sample' extension sample for img-src content security policy examples)
-				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-				<link href="${styleResetUri}" rel="stylesheet">
-				<link href="${styleVSCodeUri}" rel="stylesheet">
-				<link href="${styleMainUri}" rel="stylesheet">
-
-				<title>Cat Colors</title>
-			</head>
-			<body>
-                <H1> Test </H1>
-			</body>
-			</html>`;
-	}
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<!--
+				Use a content security policy to only allow loading images from https or from our extension directory,
+				and only allow scripts that have a specific nonce.
+	-->
+	<meta http-equiv="Content-Security-Policy" content="img-src ${webview.cspSource} https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link href="${styleResetUri}" rel="stylesheet">
+			<link href="${customCSSUri}" rel="stylesheet">
+	<link href="${styleMainUri}" rel="stylesheet">
+		</head>
+  <body>
+			<script nonce="${nonce}" src="${scriptUri}"></script>
+		</body>
+		</html>`;
+}
 }
---
+++ webviews/components/artemusChatPanel.svelte
@@ -0,0 +1,250 @@
+<script lang="ts">
+	let inputValue = '';
+	let disabled = true;
+	let inputHeight = "3em";
+	let imgSource = "";
+	// function keyup(event){
+	// 	if(disabled) 
+	// 		return
+	// 	if(event.keyCode===13 & !event.shiftKey){
+	// 		sendMessage();
+	// 	}
+	// }
+	
+  function handleInput(event:any) {
+    inputValue = event.target.value
+		disabled = true;
+		if(inputValue.trim())
+			disabled = false;
+	}
+
+  function sendMessage() {
+    // Add your logic to handle sending the message
+    // For example, you can use the 'inputValue' variable to get the user's input.
+    console.log("Sending message:", inputValue);
+		inputValue="";
+		disabled=true;
+		document.getElementById("InputTextArea")?.focus()
+  }
+</script>
+
+<style>
+	/* width */
+	::-webkit-scrollbar {
+		width: 6px;
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: transparent;
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #888;
+	}
+
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #555;
+	}
+
+	.chat-container {
+		position: -webkit-sticky; /* Safari */
+		position: sticky;
+	}
+
+	.textarea-container {
+		margin: 10px;
+		position: relative;
+	}
+
+	.send-button {
+	background-color: darkcyan;
+	border: None;
+	position: absolute;
+	bottom: 15px;
+	right: 10px;
+	padding: 5px;
+	border-radius: 5px;	
+	}
+	.send-button:focus {
+		outline: none;
+		border: 1px solid white;
+	}
+
+	.send-button:hover:not([disabled])  {
+		cursor: pointer;
+	}
+
+	button:disabled{
+		background-color: gray;
+	}
+	.output-area{
+		height: 100%;
+		overflow-y: scroll;
+		margin: 10px;
+	}
+	.input-area{
+		padding-top: 14px;
+		padding-top: 14px;
+		padding-left: 10px;
+		padding-left: 10px;
+		/* padding: 12px; */
+		resize: none;
+		overflow-y: scrollbar;
+		height: 3em;
+		min-height: 3em;
+		max-height: 15em;
+		width: 100%;
+		background: rgb(59, 58, 58);
+		color: whitesmoke;
+		font-family: 'Courier New', Courier, monospace;
+		font-size: medium;
+		border: none;
+		border-radius: 10px;
+	}
+
+	::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+		color: gray;
+		opacity: 1; /* Firefox */
+	}
+
+	.input-area:focus{
+		outline: none;
+	}
+
+	.flex-container {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+</style>
+
+
+<div class="flex-container">
+	<div class='output-area'>
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>sdfsdf</p>	
+	<p>{@html "testing markdown ```py<br> def print():\n```"}</p>
+	</div>
+	
+	<div class="chat-container">
+	  <div class="textarea-container">
+	    <textarea id='InputTextArea' class='input-area' placeholder="Ask Artemus" bind:value={inputValue} on:input={handleInput} />
+	    <button class="send-button" {disabled} on:click={sendMessage}>Send
+		</button>
+	  </div>
+	</div>
+	<!-- <div><p>Current Open File Goes Here </p></div> -->
+	<!-- <br> -->
+	<img src="" alt='Not Found'>
+</div>
+
