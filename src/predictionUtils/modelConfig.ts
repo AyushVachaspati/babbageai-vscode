cdff3b80db3a3da8c376e173597e5cd5f6fd866b
avach <avachaspati97@gmail.com>
2023-11-26 21:59:49 +0530

    fixed typos

--- src/predictionUtils/modelConfig.ts
+++ src/predictionUtils/modelConfig.ts
@@ -1,19 +1,19 @@
-
 export const currentCompletionModel = "santacoder";
 export const currentChatModel = "santacoderChat";
+
 export const modelConfig = {
     santacoder: {
         name: "santacoder_huggingface",
         prefixToken: "<fim-prefix>",
-        suffixToken: "<fim-suffix",
-        middleToken: "fim-middle"
+        suffixToken: "<fim-suffix>",
+        middleToken: "<fim-middle>"
     },
     
     starcoder: {
         name: "starcoder_huggingface",
         prefixToken: "<fim_prefix>",
-        suffixToken: "<fim_suffix",
-        middleToken: "fim_middle"
+        suffixToken: "<fim_suffix>",
+        middleToken: "<fim_middle>"
     },
 
     santacoderChat: {
